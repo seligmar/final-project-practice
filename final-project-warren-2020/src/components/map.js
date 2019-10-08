269fdcdda0f00f40753078bcc61cceb180a4f88a
github username <github email address>
2019-10-08 18:26:51 +0100

    basic map functionality is working

--- final-project-warren-2020/src/components/map.js
+++ final-project-warren-2020/src/components/map.js
@@ -1,37 +1,36 @@
-// import React from 'react'
-// import {
-//   GoogleMap,
-//   withScriptjs,
-//   withGoogleMap
-//    ,
-//   Marker,
-//   InfoWindow
-// } from 'react-google-maps'
+import React from 'react'
+import {
+  GoogleMap,
+  withScriptjs,
+  withGoogleMap,
+  Marker,
+  InfoWindow
+} from 'react-google-maps'
 
-// // Iowa per https://www.latlong.net/
-// // lat: 41.878002
-// // lng: -93.097702
+// Iowa per https://www.latlong.net/
+// lat: 41.878002
+// lng: -93.097702
 
-// const GOOGLE_API_KEY = 'AIzaSyBuNd5baj7zHX5OmBtTYoBkhW_a4WN81S8'
+const GOOGLE_API_KEY = 'AIzaSyBuNd5baj7zHX5OmBtTYoBkhW_a4WN81S8'
 
-// const Map = () => {
-//   return <GoogleMap defaultZoom={10} defaultCenter={{ lat: 41, lng: -93 }} />
-// }
+const Map = () => {
+  return <GoogleMap defaultZoom={10} defaultCenter={{ lat: 41, lng: -93 }} />
+}
 
-// const WrappedMap = withScriptjs(withGoogleMap(Map))
+const WrappedMap = withScriptjs(withGoogleMap(Map))
 
-// const MapFragment = () => {
-//   return (
-//     <div style={{ width: '50vw', height: '50vh' }}>
-//       <button onClick={() => this.getEvents()} />
-//       <WrappedMap
-//         googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${GOOGLE_API_KEY}`}
-//         loadingElement={<div style={{ height: '100%' }} />}
-//         containerElement={<div style={{ height: '100%' }} />}
-//         mapElement={<div style={{ height: '100%' }} />}
-//       />
-//     </div>
-//   )
-// }
+const MapFragment = () => {
+  return (
+    <div style={{ width: '50vw', height: '50vh' }}>
+      <button onClick={() => this.getEvents()} />
+      <WrappedMap
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${GOOGLE_API_KEY}`}
+        loadingElement={<div style={{ height: '100%' }} />}
+        containerElement={<div style={{ height: '100%' }} />}
+        mapElement={<div style={{ height: '100%' }} />}
+      />
+    </div>
+  )
+}
 
-// export default MapFragment
+export default MapFragment
