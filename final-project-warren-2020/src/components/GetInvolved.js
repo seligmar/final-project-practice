16d0a5b96f88cca8bece90eba82dcf95dce1b045
github username <github email address>
2019-10-09 14:31:31 +0100

    google civic api now working from form

--- final-project-warren-2020/src/components/GetInvolved.js
+++ final-project-warren-2020/src/components/GetInvolved.js
@@ -2,13 +2,16 @@ import React from 'react'
 import MapFragment from './Map'
 import EnterAddressForm from './EnterAddressForm'
 
+const GOOGLE_API_KEY = 'AIzaSyBuNd5baj7zHX5OmBtTYoBkhW_a4WN81S8'
+
 class GetInvolved extends React.Component {
   state = {
     events: [],
     showEvent: false,
     showMap: false,
     getRepInfo: false,
-    user: false
+    user: false,
+    reps: []
   }
 
   getEvents = () => {
@@ -31,7 +34,30 @@ class GetInvolved extends React.Component {
     this.setState({ getRepInfo: true })
   }
 
-  callGoogleAPI = e => {}
+  callGoogleAPI = e => {
+    e.preventDefault()
+    const address = []
+    let newAddress = []
+    const line1 = e.target.addressLine1.value
+    const city = e.target.city.value
+    const state = e.target.state.value
+    const zip = e.target.zip.value
+    newAddress = address.concat(
+      line1.replace(/[^\w ]/, '').split(' '),
+      city.replace(/[^\w ]/, '').split(' '),
+      state.replace(/[^\w ]/, '').split(' '),
+      zip.replace(/[^\w ]/, '').split(' ')
+    )
+    this.getRepsFromAPI(newAddress.join('+'))
+  }
+
+  getRepsFromAPI = data => {
+    return fetch(
+      `https://www.googleapis.com/civicinfo/v2/representatives?address=${data}&includeOffices=true&key=${GOOGLE_API_KEY}`
+    )
+      .then(resp => resp.json())
+      .then(reps => this.setState({ reps }))
+  }
 
   render () {
     return (
