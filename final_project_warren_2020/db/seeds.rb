4746bb1ab568e4386fd37be35a742e5dea786801
github username <github email address>
2019-10-09 11:02:45 +0100

    created new seed data events now 10

--- final_project_warren_2020/db/seeds.rb
+++ final_project_warren_2020/db/seeds.rb
@@ -16,29 +16,127 @@ users = [
 ]
 
 events = [
-  { start_time: 600,
-    end_time: 1000, 
-    date: 10/15/2019, 
+  { start_time: '06:00:00',
+    end_time: '10:00:00', 
+    date: 20019-10-25, 
     street_address_1: '69 Division Ave S', 
     city: 'Grand Rapids', 
     state: 'MI', 
     zip: '49423', 
     lat: 42.960980, 
     lng: -85.670710, 
-    title: 'Debate Watch Party'
+    title: 'DEBATE WATCH PARTY'
   },
 
   {
-    title: 'Knocktober', 
+    title: 'KNOCKTOBER', 
     state: 'MI', 
-    start_time: 100, 
-    end_time: 300, 
+    date: 2019-10-28,
+    start_time: '01:00:00', 
+    end_time: '03:00:00', 
     street_address_1: '1050 Iroquois Dr SE', 
     city: 'Grand Rapids', 
     zip: '49506', 
     lat: 42.944670, 
     lng: -85.633370
-  }
+  }, 
+
+  { start_time: '10:30:00',
+  end_time: '12:00:00', 
+  date: 20019-10-30, 
+  street_address_1: '165 Western Ave N', 
+  city: 'Saint Paul', 
+  state: 'MN', 
+  zip: '55102', 
+  lat: 44.946289, 
+  lng: -93.116524, 
+  title: 'MN FOR WARREN: COFFEE HOUR AT NINA\'S'
+},
+
+{ start_time: '09:00:00',
+end_time: '11:00:00', 
+date: 20019-10-25, 
+street_address_1: '700 W Cliff Dr', 
+city: 'Santa Cruz', 
+state: 'CA', 
+zip: '95060', 
+lat: 36.953280, 
+lng: -122.040470, 
+title: 'WAGS FOR WARREN ON WEST CLIFF WEDNESDAY!'
+},
+
+{ start_time: '09:30:00',
+end_time: '11:00:00', 
+date: 20019-10-23, 
+street_address_1: '6051 Maple St', 
+city: 'Omaha', 
+state: 'NE', 
+zip: '68104', 
+lat: 41.284700, 
+lng: -96.006140, 
+title: 'WEDNESDAYS FOR WARREN'
+},
+
+{ start_time: '18:30:00',
+end_time: '21:00:00', 
+date: 20019-10-23, 
+street_address_1: '8346 Leesburg Pike', 
+city: 'Tysons', 
+state: 'VA', 
+zip: '22182', 
+lat: 38.921240, 
+lng: -77.232300, 
+title: 'WARREN HAPPY HOUR MEET AND GREET - TYSONS'
+},
+
+{ start_time: '17:30:00',
+end_time: '19:00:00', 
+date: 20019-10-23, 
+street_address_1: '421 W 27th St NW', 
+city: 'Winston-Salem', 
+state: 'NC', 
+zip: '27105', 
+lat: 36.124180, 
+lng: -80.253630, 
+title: 'WINSTON-SALEM FAIR CANVASS / FOOD DONATION'
+},
+
+{ start_time: '18:00:00',
+end_time: '21:00:00', 
+date: 20019-11-03, 
+street_address_1: '11 Meserole St', 
+city: 'Brooklyn', 
+state: 'NY', 
+zip: '11206', 
+lat: 40.707661, 
+lng: -73.949928, 
+title: 'NORTH BROOKLYN FOR WARREN PHONE BANK'
+},
+
+{ start_time: '18:00:00',
+end_time: '20:00:00', 
+date: 20019-11-29, 
+street_address_1: '3500 S Cedar St', 
+city: 'Lansing', 
+state: 'MI', 
+zip: '48910', 
+lat: 42.694430, 
+lng: -84.548060, 
+title: 'LANSING FOR WARREN MONTHLY ORGANIZING MEETING'
+},
+
+{ start_time: '16:30:00',
+end_time: '18:30:00', 
+date: 20019-10-23, 
+street_address_1: '2633 Harris St', 
+city: 'Eugene', 
+state: 'OR', 
+zip: '97405', 
+lat: 44.029820, 
+lng: -123.077420, 
+title: 'LANE COUNTY FOR ELIZABETH WARREN - WARREN WEDNESDAY'
+},
+
 ]
 
 
