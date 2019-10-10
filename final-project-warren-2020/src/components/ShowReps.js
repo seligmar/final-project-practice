ffd8da11fb2d81a69cade408dfac6dd216ad2261
github username <github email address>
2019-10-10 10:55:26 +0100

    added semantic ui and react router not yet implimented

--- final-project-warren-2020/src/components/ShowReps.js
+++ final-project-warren-2020/src/components/ShowReps.js
@@ -3,30 +3,30 @@ import React from 'react'
 const ShowReps = props => {
   return (
     <div className='show-reps'>
-      <p>
+      <div>
         {props.reps.offices[2].name}: {props.reps.officials[2].name} Call:{' '}
         {props.reps.officials[2].phones[0]}
-      </p>
-      <p>
+      </div>
+      <div>
         {props.reps.offices[2].name}:{props.reps.officials[3].name} Call:{' '}
         {props.reps.officials[3].phones[0]}
-      </p>
-      <p>
+      </div>
+      <div>
         {props.reps.offices[3].name}: {props.reps.officials[4].name} Call:{' '}
         {props.reps.officials[4].phones[0]}
-      </p>
-      <p>
+      </div>
+      <div>
         {props.reps.offices[4].name}: {props.reps.officials[5].name} Call:{' '}
         {props.reps.officials[5].phones[0]}
-      </p>
-      <p>
+      </div>
+      <div>
         {props.reps.offices[5].name}: {props.reps.officials[6].name} Call:{' '}
         {props.reps.officials[6].phones[0]}
-      </p>
-      <p>
+      </div>
+      <div>
         {props.reps.offices[6].name}: {props.reps.officials[7].name} Call:{' '}
         {props.reps.officials[7].phones[0]}
-      </p>
+      </div>
     </div>
   )
 }
