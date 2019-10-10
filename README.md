1fc4772feb9b97a2cdde8204196beb1d50715703
github username <github email address>
2019-10-10 18:57:01 +0100

    added first quote component to homepage

--- README.md
+++ README.md
@@ -17,3 +17,5 @@ https://ballotpedia.org/API-documentation - this would probably be a better API 
 https://www.youtube.com/watch?v=Law7wfdg_ls - react router youtube video 
 
 https://www.vogue.com/article/elizabeth-warren-shares-pregnancy-discrimination-stories-twitter?utm_source=nl&utm_brand=vogue&utm_mailing=VOG_Daily_100919&utm_campaign=aud-dev&utm_medium=email&bxid=5bd6729c3f92a41245dd7811&cndid=29156881&hasha=fcccf3625e39e26e299d96691b2c1a1b&hashb=9e6ca99dedb815c7deecaf6b88186f041c580242&hashc=1f7f161038b608742d7b0efc8bdcde93dd892cff7bc6c340817f2145ffabfcb3&esrc=CM_CRM_2013&utm_term=VOG_Daily&verso=true
+
+https://www.harpersbazaar.com/culture/politics/a29008478/elizabeth-warren-quotes/
--- final-project-warren-2020/src/components/Home.js
+++ final-project-warren-2020/src/components/Home.js
@@ -26,7 +26,23 @@ class Home extends React.Component {
             loop='true'
             width='100%'
             height='0%'
-          /></div>
+          />
+        </div>
+        <div className='quote-grid'>
+          <div className='quote-img'>
+            <img className='img' src='https://www.usnews.com/dims4/USNEWS/692809a/2147483647/crop/4050x2658%2B0%2B3/resize/640x420/quality/85/?url=http%3A%2F%2Fcom-usnews-beam-media.s3.amazonaws.com%2F94%2F29%2Fe66a0d5349cd8c3dbbce6ac04d92%2F190911-2020warren-editorial.warren.JPG' />
+          </div>
+          <div className='grid-quote'>
+            <h2 className='quote-text'>
+              <p>There's a lot at stake for our democracy.
+              But we can't choose a candidate we don't believe in just because we're afraid.</p>
+              <p>I'm not afraid.</p>
+              <p>― Elizabeth Warren</p></h2>
+            <h1></h1>
+            <h1></h1>
+            <h1></h1>
+          </div>
+        </div>
       </div>
     )
   }
