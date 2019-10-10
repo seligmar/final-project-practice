fe24eab29d92bbbbf62611b10d27dd802f32642f
github username <github email address>
2019-10-10 12:45:27 +0100

    donations bar now beautiful

--- final-project-warren-2020/src/App.js
+++ final-project-warren-2020/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import './App.css'
 import GetInvolved from './components/GetInvolved'
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Link } from 'react-router-dom'
 import About from './components/About'
 import Home from './components/Home'
+import Donate from './components/Donate'
 
 // below is mockup
 // "https://www.figma.com/file/KY3BKNojNY9CZgudaWBWw5/Warren2020?node-id=8%3A1"
@@ -21,7 +22,9 @@ class App extends React.Component {
     username: ''
   }
 
-  linkTo = () => { }
+  resetDonationsBar = () => {
+    this.setState({ donationsBar: !this.state.donationsBar })
+  }
 
   closeGive = () => {
     this.setState({ donationsBar: !this.state.donationsBar })
@@ -31,22 +34,27 @@ class App extends React.Component {
     return (
       <div className='App'>
         {this.state.donationsBar ? (
-          <div className='donationsBar'>
-            <button className='ui button donations-button' onClick={e => this.linkTo(e)}>Give Now</button>
-            <button className='little-x-button' onClick={e => this.closeGive(e)}>X</button>
-          </div>
+          <div className='maybe-later' onClick={e => this.closeGive(e)}>Maybe Later</div>
         ) : null}
-
+        {this.state.donationsBar ? (
+          <Link to="/donate/elizabethwarren2020">
+            <div className='donationsBar' onClick={e => this.resetDonationsBar(e)}>
+              <h1 className="giving-text">Give Now</h1></div>
+          </Link>
+        ) : null}
         <Switch>
           <Route path='/elizabethwarren2020' component={routerProps =>
             <Home {...routerProps} username={this.state.username} />} />
           <Route path='/getinvolved' component={routerProps =>
             <GetInvolved {...routerProps} username={this.state.username} />} />
 
-          <Route path="/about" component={routerProps =>
+          <Route path="/about/elizabethwarren2020" component={routerProps =>
             <About {...routerProps} />} />
+          <Route path="/donate/elizabethwarren2020" component={routerProps =>
+            <Donate {...routerProps} />} />
+          <Route component={() => <h1>404 - Page Not Found</h1>} />
         </Switch>
-      </div>
+      </div >
     )
   }
 }
---
+++ final-project-warren-2020/src/components/Donate.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactPlayer from 'react-player'
+
+class Donate extends React.Component {
+  render() {
+    return <div />
+  }
+}
+
+export default Donate
