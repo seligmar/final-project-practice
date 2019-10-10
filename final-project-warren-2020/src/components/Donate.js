3b889641d49dcc63456ee60e44cdaa43f9cbbbab
github username <github email address>
2019-10-10 14:00:26 +0100

    donations form actually hits post request on click

--- final-project-warren-2020/src/components/Donate.js
+++ final-project-warren-2020/src/components/Donate.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom'
 class Donate extends React.Component {
 
   donateNew = e => {
+    debugger
     return fetch('http://localhost:3001/donate', {
       method: 'POST',
       headers: {
@@ -12,42 +13,37 @@ class Donate extends React.Component {
       },
       body: JSON.stringify({
         // user_id: user_id, //this needs to be user_id 
-        password: e.target.total.value
+        total: e.target.parentElement.childNodes[15].value
       })
     }).then(resp => resp.json())
   }
 
-
   render() {
     return (
       <div className='login'>
-        <form class="ui form">
-          <div class="field">
-            <label>Address</label>
-            <div class="fields">
-              <div class="twelve wide field">
-                <input type="text" name="address" placeholder="Street Address" />
-              </div>
-              <div class="four wide field">
-                <input type="text" name="address-2" placeholder="Apt #" />
-              </div >
-            </div >
+        <form class="ui form" onSubmit={e => this.donateNew(e)}>
+          <label>First Name</label>
+          <input type="text" name="name" placeholder="First Name" />
+          <label>Last Name</label>
+          <input type="text" name="surname" placeholder="Last Name" />
+          <label>Address</label>
+          <input type="text" name="address" placeholder="Street Address" />
+          <div class="four wide field">
+            <input type="text" name="address-2" placeholder="Apt #" />
           </div >
-          < h4 class="ui dividing header" > Billing Information</h4 >
-          <div class="fields">
-            <div class="seven wide field">
-              <label>Card Number</label>
-              <input type="text" name="card[number]" maxlength="16" placeholder="Card #" />
-            </div>  </div>
+          < h4> Billing Information</h4>
+          <div class="seven wide field">
+            <label>Card Number</label>
+            <input type="text" name="cardnumber" maxlength="16" placeholder="Card #" />
+          </div>
           <div class="three wide field">
             <label>CVC</label>
-            <input type="text" name="card[cvc]" maxlength="3" placeholder="CVC" />
+            <input type="text" name="cvc" maxlength="3" placeholder="CVC" />
           </div>
-          < div class="six wide field" >
-            <label>Expiration</label></div >
+          <label>Expiration</label>
           <div class="two fields">
             <div class="field">
-              <select class="ui fluid search dropdown" name="card[expire-month]">
+              <select class="ui fluid search dropdown" name="expire-month">
                 <option value="">Month</option>
                 <option value="1">January</option>
                 <option value="2">February</option>
@@ -63,12 +59,13 @@ class Donate extends React.Component {
                 <option value="12">December</option>
               </select>
             </div></div>
-          <div class="field">
-            <input type="text" name="card[expire-year]" maxlength="4" placeholder="Year" />
-          </div>
-          <div class="ui button" tabindex="0">Submit Order</div>
+          <input type="text" name="expire-year" maxlength="4" placeholder="Year" />
+          <br></br><br></br>
+          <input type="float" name="total" maxlength="7" placeholder="total" />
+          <br></br><br></br>
+          <div class="button" onClick={(e) => this.donateNew(e)}>Donate Now</div>
         </form>
-      </div>
+      </div >
     )
   }
 }
