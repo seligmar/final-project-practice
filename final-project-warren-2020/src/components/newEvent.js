f8f1cd7246fb3d5bb7c5fbb3d3f6b800a037d6e1
github username <github email address>
2019-10-15 11:58:50 +0100

    google geocode API pulling data

--- final-project-warren-2020/src/components/newEvent.js
+++ final-project-warren-2020/src/components/newEvent.js
@@ -34,16 +34,16 @@ class NewEvent extends React.Component {
         zip.replace(/[^\w ]/, '').split(' ')
       )
       const eventData = {
-        title: e.target.title,
-        start_time: e.target.start_time,
-        end_time: e.target.end_time,
+        title: e.target.title.value,
+        start_time: e.target.start.value,
+        end_time: e.target.end.value,
         address_line_1: line1,
         city: city,
         state: state,
         zip: zip,
-        day: e.target.day,
-        month: e.target.month,
-        year: e.target.year
+        day: e.target.day.value,
+        month: e.target.month.value,
+        year: e.target.year.value
       }
       this.getLatLngFromAPI(newAddress.join('+'))
       this.mergeInfo(eventData)
@@ -51,17 +51,44 @@ class NewEvent extends React.Component {
   }
 
   getLatLngFromAPI = address => {
-    debugger
-    return <div></div>
+    return fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${GOOGLE_API_KEY}`)
+      .then(resp => resp.json())
+      .then(data => this.parseAPI(data))
   }
 
-  mergeInfo = (eventData) => {
-    const allInfo = eventData
+  parseAPI = data => {
+    const lat = data["results"]["0"]["geometry"]["location"]["lat"]
+    const lng = data["results"]["0"]["geometry"]["location"]["lng"]
+    const latLng = {
+      lat: lat,
+      lng: lng
+    }
+    this.mergeInfo(latLng)
+  }
 
+  mergeInfo = (eventData, latLng) => {
+    const newEvent = Object.assign(eventData, latLng)
+    this.postEvent(newEvent)
   }
 
   postEvent = event => {
-    API.newEvent()
+    API.newEvent(event)
+      .then(data => {
+        if (data.error) {
+          this.responseGif(data.error)
+        } else {
+          console.log(data)
+        }
+      })
+  }
+
+  responseGif = (response) => {
+    MySwal.fire({
+      title: 'Please try again',
+      text: `${response}`,
+      confirmButtonColor: '#b61b28',
+      animation: false
+    })
   }
 
   render() {
@@ -80,35 +107,41 @@ class NewEvent extends React.Component {
             <label>Start Time</label>
             <input
               style={{ width: 200 }}
-              type='time'
-              name='start-time'
-              placeholder='Start Time'
+              type='text'
+              name='start'
+              placeholder='Start Time 00:00'
             />
             <label>End Time</label>
-            <input
-              style={{ width: 200 }}
-              type='time'
-              name='end-time'
-              placeholder='End Time'
-            />
-            <input
-              style={{ width: 200 }}
-              type='text'
-              name='day'
-              placeholder='DD'
-            />
-            <input
-              style={{ width: 200 }}
-              type='text'
-              name='month'
-              placeholder='MM'
-            />
-            <input
-              style={{ width: 200 }}
-              type='text'
-              name='year'
-              placeholder='YYYY'
-            />
+            <div class="fields">
+              <div class="two wide field"></div>
+              <input
+                style={{ width: 200 }}
+                type='text'
+                name='end'
+                placeholder='End Time 00:00'
+              />
+              <input
+                style={{ width: 200 }}
+                type='text'
+                name='day'
+                placeholder='DD'
+              /></div>
+            <div class="fields">
+              <div class="two wide field"></div>
+              <input
+                style={{ width: 200 }}
+                type='text'
+                name='month'
+                placeholder='MM'
+              /> </div>
+            <div class="fields">
+              <div class="four wide field"></div>
+              <input
+                style={{ width: 200 }}
+                type='text'
+                name='year'
+                placeholder='YYYY'
+              /></div>
             <label>Please Enter the Address Where the Event Will Occor:</label>
             <input
               style={{ width: 200 }}
@@ -137,7 +170,7 @@ class NewEvent extends React.Component {
             <button className='ui button' type='submit'>Submit</button>
           </div>
         </form>
-      </div>
+      </div >
     )
   }
 }
--- final_project_warren_2020/app/controllers/events_controller.rb
+++ final_project_warren_2020/app/controllers/events_controller.rb
@@ -24,26 +24,19 @@ class EventsController < ApplicationController
   end 
 
   def create 
-    new_event = {
-      start_time: event.start_time.strftime("%H:%M"), 
-      end_time: event.end_time.strftime("%H:%M"), 
-      day: event.day,
-      month: event.month, 
-      year: event.year,
-      street_address_1: event.street_address_1,
-      street_address_2: event.street_address_2,
-      city: event.city,
-      state: event.state,
-      zip: event.zip,
-      title: event.title,
-      lng: event.lng,
-      lat: event.lat}
-      
       event = Event.new(
-        username: params[:username], 
-        password: params[:password], 
-        emailaddress: params[:emailaddress]
-        
+        start_time: params[:start_time].strftime("%H:%M"), 
+        end_time: params[:end_time].strftime("%H:%M"), 
+        day: params[:day],
+        month: params[:month], 
+        year: params[:year],
+        street_address_1: params[:street_address_1],
+        city: params[:city],
+        state: params[:state],
+        zip: params[:zip],
+        title: params[:title],
+        lng: params[:lng],
+        lat: params[:lat]
         )
       if event.save
           render json: event, status: :create
@@ -64,7 +57,6 @@ class EventsController < ApplicationController
     :month, 
     :year, 
     street_address_1, 
-    :street_address_2, 
     :city, 
     :state, 
     :zip, 
