e102f656efa821bd07a56cc354fb3bfa4591c059
github username <github email address>
2019-10-16 09:17:32 +0100

    centered social media buttons

--- final-project-warren-2020/src/Menu.js
+++ final-project-warren-2020/src/Menu.js
@@ -21,40 +21,36 @@ class Menu extends React.Component {
           </Link>
         </div>
         <div>
-          <div className='social-media-grid'>
-            <div className='social-media'>
-              <a href="https://www.facebook.com/ElizabethWarren/" target="_blank">
-                <button class="ui facebook button">
-                  <i class="facebook icon"></i>
-                  Facebook
-            </button></a>
-              <a href='https://twitter.com/ewarren' target="_blank">
-                <button class="ui twitter button">
-                  <i class="twitter icon"></i>
-                  Twitter
-</button></a>
-              <a href='https://www.instagram.com/elizabethwarren/' target="_blank">
-                <button class="ui instagram button">
-                  <i class="instagram icon"></i>
-                  Instagram
-</button></a>
-              <a href='https://www.youtube.com/channel/UCrj-0FxK19tgUR2EbHkBBGg' target="_blank">
-                <button class="ui youtube button">
-                  <i class="youtube icon"></i>
-                  YouTube
-</button></a>
-            </div></div>
           <div className='menu-gif'>
             <iframe src="https://giphy.com/embed/LqrDQpeG0ojjfEjKCb"
               // width="480" 
               height="480"
               frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div>
+          <div className='four ui icon buttons'>
+            <div className='social-media'>
+              <a href="https://www.facebook.com/ElizabethWarren/" target="_blank">
+                <button class="ui facebook button">
+                  <i class="facebook icon"></i>
+                  Facebook
+</button></a>
+              <a href='https://twitter.com/ewarren' target="_blank">
+                <button class="ui twitter button">
+                  <i class="twitter icon"></i>Twitter</button></a>
+              <a href='https://www.instagram.com/elizabethwarren/' target="_blank">
+                <button class="ui instagram button">
+                  <i class="instagram icon"></i>Instagram</button></a>
+              <a href='https://www.youtube.com/channel/UCrj-0FxK19tgUR2EbHkBBGg' target="_blank">
+                <button class="ui  youtube button">
+                  <i class="youtube icon"></i>
+                  YouTube</button></a>
+            </div>
+          </div>
           <p>PAID FOR BY WARREN FOR PRESIDENT.</p>
           <p> © Copyright 2019. All rights reserved.</p></div>
         <p></p>
-      </div>
+      </div >
     )
   }
 }
 
-export default Menu 
+export default Menu
--- final-project-warren-2020/src/components/Home.js
+++ final-project-warren-2020/src/components/Home.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactPlayer from 'react-player'
 import { Link } from 'react-router-dom'
-import { TwitterTimelineEmbed, TwitterShareButton, TwitterFollowButton, TwitterHashtagButton, TwitterMentionButton, TwitterTweetEmbed, TwitterMomentShare, TwitterDMButton, TwitterVideoEmbed, TwitterOnAirButton } from 'react-twitter-embed';
+import { TwitterTimelineEmbed } from 'react-twitter-embed'
+
+// import { TwitterTimelineEmbed, TwitterShareButton, TwitterFollowButton, TwitterHashtagButton, TwitterMentionButton, TwitterTweetEmbed, TwitterMomentShare, TwitterDMButton, TwitterVideoEmbed, TwitterOnAirButton } from 'react-twitter-embed';
 
 class Home extends React.Component {
   render() {
