c7677848dc9d51e73e2f5c1a524b1d866f230af3
github username <github email address>
2019-10-17 13:26:22 +0100

    finalized show reps page

--- final-project-warren-2020/src/components/ShowReps.js
+++ final-project-warren-2020/src/components/ShowReps.js
@@ -3,26 +3,40 @@ import React from 'react'
 const ShowReps = props => {
   return (
     <div className='show-reps'>
+      <div>
+        <div className='rep-names'>  <h4>{props.reps.offices[2].name}: </h4>
+          {props.reps.officials[2].name}
+          <p>Call:{' '}
+            {props.reps.officials[2].phones[0]}</p></div>
+      </div>
+      <div>
+        <div className='rep-names'> <h4>  {props.reps.offices[2].name}: </h4>
+          {props.reps.officials[3].name}
+          <p>Call:{' '}
+            {props.reps.officials[3].phones[0]}</p></div>
+      </div>
+      <div>
+        <div className='rep-names'>
+          <h4> {props.reps.offices[3].name}: </h4>
+          {props.reps.officials[4].name}
+          <p>Call:{' '}
+            {props.reps.officials[4].phones[0]}</p></div>
+      </div>
+      <div>
+        <div className='rep-names'>
+          <h4>{props.reps.offices[4].name}: </h4>
+          {props.reps.officials[5].name}
+          <p>Call:{' '}
+            {props.reps.officials[5].phones[0]}</p></div>
+      </div>
+      <div>
+        <div className='rep-names'>
+          <h4> {props.reps.offices[5].name}: </h4>
+          {props.reps.officials[6].name}
+          <p>Call:{' '}
+            {props.reps.officials[6].phones[0]}</p></div>
+      </div>
       <div>Please note that information may not be accurate for U.S. territories or Washington, D.C.</div>
-      <div>{props.reps.offices[2].name}: {props.reps.officials[2].name} Call:{' '}
-        {props.reps.officials[2].phones[0]}
-      </div>
-      <div>
-        {props.reps.offices[2].name}:{props.reps.officials[3].name} Call:{' '}
-        {props.reps.officials[3].phones[0]}
-      </div>
-      <div>
-        {props.reps.offices[3].name}: {props.reps.officials[4].name} Call:{' '}
-        {props.reps.officials[4].phones[0]}
-      </div>
-      <div>
-        {props.reps.offices[4].name}: {props.reps.officials[5].name} Call:{' '}
-        {props.reps.officials[5].phones[0]}
-      </div>
-      <div>
-        {props.reps.offices[5].name}: {props.reps.officials[6].name} Call:{' '}
-        {props.reps.officials[6].phones[0]}
-      </div>
     </div >
   )
 }
